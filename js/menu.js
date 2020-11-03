4b04027e35cb5c271d3c08db038d2cedf75872f3
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2020-11-03 17:23:35 -0300

    HTML, CSS OK

--- js/menu.js
+++ js/menu.js
@@ -2,18 +2,16 @@ var burger = document.querySelector(".burgerBtn");
 var menu = document.querySelector(".nav-links");
 var linkHome = document.querySelector(".liHome");
 var linkAbout = document.querySelector(".liAbout");
-var linkProductos = document.querySelector(".liProductos");
+var linkServicios = document.querySelector(".liServicios");
 var linkContacto = document.querySelector(".liContacto");
-var linkFaq = document.querySelector(".liFaq");
 
 burger.addEventListener("click", function abrirMenu() {
   menu.classList.toggle("active");
   burger.classList.toggle("toggle");
   linkHome.classList.toggle("link-active");
   linkAbout.classList.toggle("link-active");
-  linkProductos.classList.toggle("link-active");
+  linkServicios.classList.toggle("link-active");
   linkContacto.classList.toggle("link-active");
-  linkFaq.classList.toggle("link-active");
 });
 
 linkHome.addEventListener("click", function closeMenu() {
@@ -21,9 +19,8 @@ linkHome.addEventListener("click", function closeMenu() {
   burger.classList.remove("toggle");
   linkHome.classList.remove("link-active");
   linkAbout.classList.remove("link-active");
-  linkProductos.classList.remove("link-active");
+  linkServicios.classList.remove("link-active");
   linkContacto.classList.toggle("link-active");
-  linkFaq.classList.remove("link-active");
 });
 
 linkAbout.addEventListener("click", function closeMenu() {
@@ -31,18 +28,17 @@ linkAbout.addEventListener("click", function closeMenu() {
   burger.classList.remove("toggle");
   linkHome.classList.remove("link-active");
   linkAbout.classList.remove("link-active");
-  linkProductos.classList.remove("link-active");
+  linkServicios.classList.remove("link-active");
   linkContacto.classList.remove("link-active");
 });
 
-linkProductos.addEventListener("click", function closeMenu() {
+linkServicios.addEventListener("click", function closeMenu() {
   menu.classList.remove("active");
   burger.classList.remove("toggle");
   linkHome.classList.remove("link-active");
   linkAbout.classList.remove("link-active");
-  linkProductos.classList.remove("link-active");
+  linkServicios.classList.remove("link-active");
   linkContacto.classList.remove("link-active");
-  linkFaq.classList.remove("link-active");
 });
 
 linkContacto.addEventListener("click", function closeMenu() {
@@ -50,17 +46,6 @@ linkContacto.addEventListener("click", function closeMenu() {
   burger.classList.remove("toggle");
   linkHome.classList.remove("link-active");
   linkAbout.classList.remove("link-active");
-  linkProductos.classList.remove("link-active");
+  linkServicios.classList.remove("link-active");
   linkContacto.classList.remove("link-active");
-  linkFaq.classList.remove("link-active");
-});
-
-linkFaq.addEventListener("click", function closeMenu() {
-  menu.classList.remove("active");
-  burger.classList.remove("toggle");
-  linkHome.classList.remove("link-active");
-  linkAbout.classList.remove("link-active");
-  linkProductos.classList.remove("link-active");
-  linkContacto.classList.remove("link-active");
-  linkFaq.classList.remove("link-active");
 });
